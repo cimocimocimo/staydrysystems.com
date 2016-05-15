c00b5775b462def309863b362db1f85ee32a743f
Aaron Cimolini <aaron@cimolini.com>
2016-05-15 16:00:37 -0400

    Updates footer tagline and copyright.

--- web/app/themes/staydry-storefront/functions.php
+++ web/app/themes/staydry-storefront/functions.php
@@ -1,10 +1,12 @@
 <?php
-// load the parent style.css
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
 
-// Unhook Storefront theme credit
-remove_action('storefront_footer', 'storefront_credit', 20);
+// remove the Storefront theme credit.
+add_filter('storefront_credit_link', function(){
+    return false;
+});
+
+add_filter('storefront_copyright_text', function($value){
+    return sprintf('%s - %s', $value, get_bloginfo('description'));
+});
+
 ?>
