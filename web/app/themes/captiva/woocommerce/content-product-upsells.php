4755581dc69598f4cf20aedf0c53fd9573937314
Aaron Cimolini <aaron@cimolini.com>
2016-05-25 11:54:41 -0400

    Makes product upsell block wider.

--- web/app/themes/captiva/woocommerce/content-product-upsells.php
+++ web/app/themes/captiva/woocommerce/content-product-upsells.php
@@ -13,14 +13,15 @@ global $product, $woocommerce_loop;
 $attachment_ids = $product->get_gallery_attachment_ids();
 ?>         
 
-<li class="up-sell-product col-lg-6 col-md-6 col-sm-6">
+<li class="up-sell-product col-lg-12 col-md-12 col-sm-12">
     <a class="product-tooltip" href="<?php the_permalink(); ?>">
         <div class="product-image">
-<?php echo get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ) ) ?>
+          <?php echo get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ) ) ?>
         </div><!-- end product-image -->
+        <h5><?php the_title(); ?></h5>
     </a>
     <div class="tooltiptext">
-<?php the_title(); ?> - <?php echo $product->get_price_html(); ?>
+      <?php the_title(); ?> - <?php echo $product->get_price_html(); ?>
     </div>
 </li><!-- end product -->
 
