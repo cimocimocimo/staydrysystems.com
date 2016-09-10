2fc93f02e40aa968a121bd27720437982d006435
Aaron Cimolini <aaron@cimolini.com>
2016-09-09 23:10:24 -0700

    Adds asset-builder and manifest.json.

--- web/app/themes/staydry-storefront/gulpfile.babel.js
+++ web/app/themes/staydry-storefront/gulpfile.babel.js
@@ -7,6 +7,37 @@ import svg2png from 'gulp-svg2png';
 import size from 'gulp-size';
 import runSequence from 'run-sequence';
 import sass from 'gulp-sass';
+import merge from 'merge-stream';
+
+// See https://github.com/austinpray/asset-builder
+var manifest = require('asset-builder')('./assets/manifest.json');
+
+// `path` - Paths to base asset directories. With trailing slashes.
+// - `path.source` - Path to the source files. Default: `assets/`
+// - `path.dist` - Path to the build directory. Default: `dist/`
+var path = manifest.paths;
+
+// `config` - Store arbitrary configuration values here.
+var config = manifest.config || {};
+
+// `globs` - These ultimately end up in their respective `gulp.src`.
+// - `globs.js` - Array of asset-builder JS dependency objects. Example:
+//   ```
+//   {type: 'js', name: 'main.js', globs: []}
+//   ```
+// - `globs.css` - Array of asset-builder CSS dependency objects. Example:
+//   ```
+//   {type: 'css', name: 'main.css', globs: []}
+//   ```
+// - `globs.fonts` - Array of font path globs.
+// - `globs.images` - Array of image path globs.
+// - `globs.bower` - Array of all the main Bower files.
+var globs = manifest.globs;
+
+// `project` - paths to first-party assets.
+// - `project.js` - Array of first-party JS assets.
+// - `project.css` - Array of first-party CSS assets.
+var project = manifest.getProjectGlobs();
 
 var basePaths = {
         src: 'src/',
@@ -87,9 +118,15 @@ gulp.task('png-sprite', ['svg-sprite'], () => {
 gulp.task('sprite', ['png-sprite']);
 
 gulp.task('styles', () => {
-    return gulp.src(paths.styles.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(paths.styles.dest));
+    var merged = merge();
+    
+    manifest.forEachDependency('css', (dep) => {
+        merged.add(gulp.src(dep.globs)
+            .pipe(sass().on('error', sass.logError)));
+    });
+    
+    return merged
+        .pipe(gulp.dest(path.dist + 'styles'));
 });
 
 gulp.task('watch', () => {
