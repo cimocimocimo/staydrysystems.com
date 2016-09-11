8641d7478b0844f092bddc8da86ce5017152ea37
Aaron Cimolini <aaron@cimolini.com>
2016-09-11 16:30:32 -0700

    Styles top-bar.

--- web/app/themes/staydry-storefront/templates/top-bar.php
+++ web/app/themes/staydry-storefront/templates/top-bar.php
@@ -1,8 +1,23 @@
-<div id="top-bar" class="top-bar">
+<?php 
+
+function echo_acf_field_if_exists($acf_group, $block, $element){
+    $acf_key = preg_replace('/[-\s]+/', '_', $block . '_' . $element);
+    $css_selector = preg_replace('/[_\s]+/', '-', $block) . '__' . preg_replace('/[_\s]+/', '-', $element);
+    
+    if ( get_field($acf_key, $acf_group) ) {
+?>
+  <div class="<?php echo $css_selector ?>">
+    <?php the_field($acf_key, $acf_group); ?>
+  </div>
+<?php
+    }
+}
+
+?><div id="top-bar" class="top-bar">
   <div class="col-full">
     <?php if ( get_field( 'show_accepted_payment_logos', 'top-bar' )
             && have_rows('accepted_payment_logos', 'top-bar') ) { ?>
-      <div class="accepted-payment-logos column-4">
+      <div class="accepted-payment-logos">
         <?php while ( have_rows('accepted_payment_logos', 'top-bar') ) {
             the_row();
             $logo = get_sub_field('logo_image');
@@ -16,17 +31,9 @@
       </div>
     <?php } // endif ?>
     <?php if ( get_field('show_site_message', 'top-bar') ) { ?>
-      <div class="site-message column-8">
-        <?php if ( get_field('site_message_top_line', 'top-bar') ) { ?>
-          <div class="site-message__top-line">
-            <?php the_field('site_message_top_line', 'top-bar'); ?>
-          </div>
-        <?php } // endif ?>
-        <?php if ( get_field('site_message_bottom_line', 'top-bar') ) { ?>
-          <div class="site-message__bottom-line">
-            <?php the_field('site_message_bottom_line', 'top-bar'); ?>
-          </div>
-        <?php } // endif ?>
+      <div class="site-message">
+        <?php echo_acf_field_if_exists('top-bar', 'site message', 'first line'); ?>
+        <?php echo_acf_field_if_exists('top-bar', 'site message', 'second line'); ?>
       </div>
     <?php } // endif ?>
   </div>
