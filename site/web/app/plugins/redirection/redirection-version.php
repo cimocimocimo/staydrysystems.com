57abec9409f3a84c1d3f702ee42dd436739ef104
Aaron Cimolini <aaron@cimolini.com>
2021-01-24 22:30:12 -0800

    Plugin update

--- site/web/app/plugins/redirection/redirection-version.php
+++ site/web/app/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.7' );
-define( 'REDIRECTION_BUILD', '2ca80703d3d905fb590f039ab6403588' );
+define( 'REDIRECTION_VERSION', '4.9.2' );
+define( 'REDIRECTION_BUILD', 'b1c9e2f6a1c3f8b6cc251a44757c6863' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
